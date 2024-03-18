1a64e3a27c3ffff70c1da9aacb13fa7be3754bc8
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-03-17 19:22:31 -0700

    Add support for BEQ

--- rv_emu.c
+++ rv_emu.c
@@ -160,6 +160,12 @@ void emu_b_type(struct rv_state *rsp, uint32_t iw)
 	int32_t signed_imm = sign_extend(imm, 13);
 
 	switch (funct3) {
+	case 0b000: /* BEQ */
+		if (signed_rs1_val == signed_rs2_val)
+			rsp->pc += signed_imm; /* jump to label */
+		else
+			rsp->pc += 4; /* Next instruction */
+		break;
 	case 0b100: /* BLT */
 		if (signed_rs1_val < signed_rs2_val)
 			rsp->pc += signed_imm; /* jump to label */
